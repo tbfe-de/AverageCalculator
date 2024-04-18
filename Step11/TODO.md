02b9f760be35bc5993a53f2d18ebd987762219c0
Martin Weitzel <github@tbfe.de>
2024-04-18 07:12:37 +0200

    completed step 11

--- Step11/TODO.md
+++ Step11/TODO.md
@@ -1,9 +1,20 @@
-TODO:
+# TODO
 
-- Put the class `Averager` and the functions `do_averages` and
-  `do_single_average` into a namespace `my`` and adapt the call in
-  the main program accordingly.
-  
+- Move the class `Averager` into files of their own properly in
+  dividing it into
+  - a header file (`Averager.h`) with the class definition and
+  - an implementation file (`Averager.cpp`) for the non-`inline`
+    member and friend functions (if any).
+- Accordingly add `#include` directives to the file(s) which need
+  to know the class definition.
+
+Note: If you already compile handing ALL implementation files to
+the compiler with `*.cpp` nothing more needs to be done. In case
+you use a more sophisticated build mechanism (like *Makefiles* or
+*CMake*) you may need to adapt it, so that a new object file
+(`Averager.o`) is created by the compiler and then included in the
+linker command.
+
 -----------------------------------------------------------------
 
 TODO (optional - meant for further self-study only):
--- Step11/main.cpp
+++ Step11/main.cpp
@@ -16,7 +16,7 @@ int main() {
 #else
 #include "pxt.h"
 
-int main() {
+void typical_good_input() {
     { ////////////////////////////////////////////////////////////
     std::istringstream input{
             "1 5 7"     "\n"
@@ -26,16 +26,26 @@ int main() {
     std::ostringstream output{};
     PX(do_averages(input, output), output.str())
         >>= "4.33333"   "\n"
-             "4.25"     "\n"
-             "15.7"     "\n";
+            "4.25"      "\n"
+            "15.7"      "\n";
     } ////////////////////////////////////////////////////////////
+}
 
+void some_empty_inputs() {
     { ////////////////////////////////////////////////////////////
     std::istringstream input{"\n"};
     std::ostringstream output{};
     PX(do_averages(input, output), output.str()) == "";
     } ////////////////////////////////////////////////////////////
 
+    { ////////////////////////////////////////////////////////////
+    std::istringstream input{"     \n"};
+    std::ostringstream output{};
+    PX(do_averages(input, output), output.str()) == "";
+    } ////////////////////////////////////////////////////////////
+}
+
+void some_invalid_inputs() {
     { ////////////////////////////////////////////////////////////
     std::istringstream input{
             "1 5 7"     "\n"
@@ -44,10 +54,45 @@ int main() {
     };
     std::ostringstream output{};
     PX(do_averages(input, output), output.str())
-        >>= "4.33333"                                     "\n"
+        >>= "4.33333"                            "\n"
              "non-numeric input - line ignored"  "\n"
-             "15.7"                                       "\n";
+             "15.7"                              "\n";
     } ////////////////////////////////////////////////////////////
- }
+
+    { ////////////////////////////////////////////////////////////
+    std::istringstream input{
+            "1 5 7"     "\n"
+            "xxx 7"     "\n"
+            "1 xyz 7"   "\n"
+            "1 xyz"     "\n"
+    };
+    std::ostringstream output{};
+    PX(do_averages(input, output), output.str())
+        >>= "4.33333"                            "\n"
+             "non-numeric input - line ignored"  "\n"
+             "non-numeric input - line ignored"  "\n"
+             "non-numeric input - line ignored"  "\n";
+    } ////////////////////////////////////////////////////////////
+}
+
+void fails_on_my_mac_only() {
+    { ////////////////////////////////////////////////////////////
+    std::istringstream input{
+            "1 xyz"     "\n"
+            "1 abc"     "\n"
+    };
+    std::ostringstream output{};
+    PX(do_averages(input, output), output.str())
+        >>= "non-numeric input - line ignored"  "\n"
+            "non-numeric input - line ignored"  "\n";
+    } ////////////////////////////////////////////////////////////
+}
+
+int main() {
+    typical_good_input();
+    some_empty_inputs();
+    some_invalid_inputs();
+//  fails_on_my_mac_only();
+}
 
 #endif
