48ec593d0481e08fb12a407daf166fda185319d5
Martin Weitzel <github@tbfe.de>
2024-09-26 06:41:07 +0000

    removed `namespace my` from Averager.h

--- Step12/Averager.cpp
+++ Step12/Averager.cpp
@@ -2,13 +2,11 @@
 
 #include <iostream>
 
-namespace my {
-    std::ostream& operator<<(std::ostream& lhs, Averager const& rhs) {
-        return lhs << rhs.sum_/rhs.count_;
-    }
+std::ostream& operator<<(std::ostream& lhs, Averager const& rhs) {
+    return lhs << rhs.sum_/rhs.count_;
+}
 
-    void operator+=(Averager& lhs, float rhs) {
-        lhs.sum_ += rhs;
-        ++lhs.count_;
-    }
+void operator+=(Averager& lhs, float rhs) {
+    lhs.sum_ += rhs;
+    ++lhs.count_;
 }
